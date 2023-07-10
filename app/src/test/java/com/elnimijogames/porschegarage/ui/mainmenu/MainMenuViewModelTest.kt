dde75dae70abc515805b36bd40f6007142ae410a
nigel cardozo <games@elnimijo.com>
2023-07-10 19:22:11 +0100

    Added MainMenuViewModel unit tests

---
+++ app/src/test/java/com/elnimijogames/porschegarage/ui/mainmenu/MainMenuViewModelTest.kt
@@ -0,0 +1,46 @@
+package com.elnimijogames.porschegarage.ui.mainmenu
+
+import com.elnimijogames.porschegarage.model.MenuItem
+import com.elnimijogames.porschegarage.model.MenuItemRepository
+import com.elnimijogames.porschegarage.model.MenuPhotoGalleryRepository
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import io.mockk.impl.annotations.RelaxedMockK
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+
+class MainMenuViewModelTest {
+
+    @RelaxedMockK
+    private lateinit var galleryRepository: MenuPhotoGalleryRepository
+
+    @RelaxedMockK
+    private lateinit var menuItemRepository: MenuItemRepository
+
+    private lateinit var viewModel: MainMenuViewModel
+
+    @Before
+    fun setup() {
+        MockKAnnotations.init(this)
+        viewModel = MainMenuViewModel(galleryRepository, menuItemRepository)
+    }
+
+    @Test
+    fun testMainMenuViewModel() {
+        // Prepare mock data
+        val galleryList = listOf("image1.jpg", "image2.jpg")
+        val menuList = listOf(MenuItem("file://some/file/name1.jpg", "1"), MenuItem("file://some/file/name2.jpg", "2"))
+
+        // Mock repository methods
+        every { galleryRepository.getImageGalleryList() } returns galleryList
+        every { menuItemRepository.getMenuItemsList() } returns menuList
+
+        // create view model after defining actions of mock
+        viewModel = MainMenuViewModel(galleryRepository, menuItemRepository)
+
+        // Verify the initial state of the view model
+        assertEquals(galleryList, viewModel.photoGalleryState.value)
+        assertEquals(menuList, viewModel.itemMenuState.value)
+    }
+}
