32ae352611daddeef19244f35c8e83c0264d032c
nigel cardozo <games@elnimijo.com>
2023-07-13 21:39:30 +0100

    Tidied up

--- app/src/main/java/com/elnimijogames/porschegarage/ui/detailsscreen/DetailsScreenViewModel.kt
+++ app/src/main/java/com/elnimijogames/porschegarage/ui/detailsscreen/DetailsScreenViewModel.kt
@@ -9,11 +9,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.withStyle
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
-import com.elnimijogames.porschegarage.model.DetailItem
 import com.elnimijogames.porschegarage.model.MenuId
-import com.elnimijogames.porschegarage.model.MenuItem
-import com.elnimijogames.porschegarage.model.MenuItemRepository
-import com.elnimijogames.porschegarage.model.MenuPhotoGalleryRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import timber.log.Timber
 import javax.inject.Inject
@@ -26,23 +22,8 @@ class DetailsScreenViewModel @Inject constructor(private val savedStateHandle: S
 
     init {
         val menuId = savedStateHandle.get<String>("menuId")?: ""
-
         Timber.d("menuId == $menuId")
 
-//        if (MenuId.MENU_ID_SUMMARY == menuId) {
-//            detailsText.value = "SUMMARY"
-//            Timber.d("detailsText set to SUMMARY")
-//        } else if (MenuId.MENU_ID_HIGHLIGHTS == menuId) {
-//            detailsText.value = "HIGHLIGHTS"
-//            Timber.d("detailsText set to HIGHLIGHTS")
-//        } else if (MenuId.MENU_ID_INTERIOR == menuId) {
-//            detailsImagePath.value = "file:///android_asset/images/cars/porsche18.jpg"
-//            detailsText.value =
-//                "The Perfect Seat." + "Adapted to the driver via scans and integrated into the monocoque: the CFRP seat shells with custom upholstery, the six-point seat belt and the integrated LED light strip. The driver’s seat in Kalahari Grey also clearly stands out from the passenger seat in the contrasting colour of Andaluz Brown. An asymmetrical design that deliberately puts the driver at the heart of things." +
-//                "Performance Made to Measure" + "Optimum ergonomics for rapid lap times: the leather pads on the seats of the Mission X are individually adapted for the driver. If desired, the top pad can be removed when driving with a helmet. The pedals can also be adjusted if necessary. A scale in the footwell area ensures optimum interaction." +
-//                "Driver-orientated displays." + "Clear focus: centre screen, curved display and digital wing mirrors are all set up with the driver in mind. In addition, all essential controls are located on the driver’s axle. Also integrated into the Mission X: a Track Mode view, which clearly displays all key performance data." +
-//                "Shared Emotions" + "A touch of a button on the multi-purpose controller is all it takes: six cameras bring the track experience to life and for all to see. Three exterior and three interior cameras capture the track, driver and passenger. A highlight video is then created automatically, which can be shared directly on social media."
-//        } else if (MenuId.MENU_ID_EXTERIOR == menuId) {
         if (MenuId.MENU_ID_SUMMARY == menuId) {
             detailsTitle.value = getSummaryTitle()
             detailsImagePath.value = getSummaryImagePath()
--- app/src/main/java/com/elnimijogames/porschegarage/ui/galleryscreen/GalleryScreen.kt
+++ app/src/main/java/com/elnimijogames/porschegarage/ui/galleryscreen/GalleryScreen.kt
@@ -1,7 +1,6 @@
 package com.elnimijogames.porschegarage.ui.galleryscreen
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
