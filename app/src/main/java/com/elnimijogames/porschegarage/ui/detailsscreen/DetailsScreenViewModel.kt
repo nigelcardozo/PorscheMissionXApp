ae0a1cc268d585cc8f1ce62cfd96e7bd9208558f
nigel cardozo <games@elnimijo.com>
2023-07-15 13:03:02 +0100

    Tidying Up

--- app/src/main/java/com/elnimijogames/porschegarage/ui/detailsscreen/DetailsScreenViewModel.kt
+++ app/src/main/java/com/elnimijogames/porschegarage/ui/detailsscreen/DetailsScreenViewModel.kt
@@ -3,15 +3,10 @@ package com.elnimijogames.porschegarage.ui.detailsscreen
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.text.AnnotatedString
-import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.withStyle
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import com.elnimijogames.porschegarage.model.DetailsScreenRepository
-import com.elnimijogames.porschegarage.model.MenuId
-import com.elnimijogames.porschegarage.model.MenuItemRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import timber.log.Timber
 import javax.inject.Inject
